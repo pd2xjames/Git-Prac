8274213a5b40528a8504d06fa468bb557833531a
James Boakye <James.Boakye@pd2x.com>
2024-05-13 19:57:57 +0100

    Practise Version 3

--- change.js
+++ change.js
@@ -1,2 +1,2 @@
 console.log ('change.js');
-console.log ('Practise Version 2');
+console.log ('Practise Version 3');
--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 console.log ('config.js');
-console.log ('Practise Version 2');
+console.log ('Practise Version 3');
